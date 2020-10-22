5e7a3788e79c358dc795d43fc504620bbaf70ead
arqhive <hgystation@gmail.com>
2020-10-22 18:53:10 +0900

    화면간 params의 이동

--- App.js
+++ App.js
@@ -1,28 +1,72 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 // 스택 내비게이터 만들기
-function HomeScreen({navigation}) {
+function HomeScreen({route, navigation}) {
+  // React.useEffect(() => {
+  //   if (route.params?.post) {
+  //   }
+  // }, [route.params?.post]);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() =>
+          navigation.navigate('Details', {
+            // 직렬화가 가능한 데이터만 전달됨
+            itemId: 86,
+            otherParam: 'anything you want here',
+          })
+        }
       />
+      <Button
+        title="Create Post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{margin: 10}}>Post: {route.params?.post}</Text>
     </View>
   );
 }
 
-function DetailsScreen({navigation}) {
+function CreatePostScreen({navigation, route}) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{height: 200, padding: 10, backgroundColor: 'white'}}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          navigation.navigate('Home', {post: postText}); // 이전 화면에 params을 넘겨 줄 수도 있음
+        }}
+      />
+    </>
+  );
+}
+
+function DetailsScreen({route, navigation}) {
+  const {itemId, otherParam} = route.params;
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>DetailsScreen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')} // 이미 있는 경로지만 새로 경로를 추가한다.
+        onPress={() =>
+          navigation.push('Details', {itemId: Math.floor(Math.random() * 100)})
+        } // 이미 있는 경로지만 새로 경로를 추가한다.
       />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
@@ -44,9 +88,12 @@ export default function App() {
           component={HomeScreen}
           options={{title: 'Overview'}}
         />
-        <Stack.Screen name="Details">
+        <Stack.Screen
+          name="Details"
+          initialParams={{itemId: 42, otherParam: 'default'}}>
           {(props) => <DetailsScreen {...props} myData="myData" />}
         </Stack.Screen>
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
