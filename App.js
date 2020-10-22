1e73a988c809c718afbfd0b78dadfd3fd7fb5e5c
arqhive <hgystation@gmail.com>
2020-10-22 18:34:12 +0900

    화면 이동

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 // 스택 내비게이터 만들기
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-function DetailsScreen(props) {
+function DetailsScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>DetailsScreen</Text>
-      <Text>{JSON.stringify(props)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')} // 이미 있는 경로지만 새로 경로를 추가한다.
+      />
     </View>
   );
 }
@@ -26,7 +33,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
