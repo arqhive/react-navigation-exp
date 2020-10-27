9981ca4ac2b29e9274f4d9d5d6afca91de611c36
arqhive <hgystation@gmail.com>
2020-10-27 12:02:38 +0900

    기본 탭바 커스텀

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {View, Text, Settings} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home!</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
     </View>
   );
 }
@@ -24,8 +28,29 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? '포커스된 아이콘' : '안포커스된 아이콘';
+            } else if (route.name === 'Settings') {
+              iconName = '세팅 아이콘';
+            }
+
+            // return 아이콘 컴포넌트
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'red',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{tabBarBadge: 3}}
+        />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
